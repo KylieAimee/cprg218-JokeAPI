bda39146e6241c76f6615f403f25246de92b00b7
KylieAimee <kylie.farrell@edu.sait.ca>
2025-03-23 13:35:22 -0600

    programming joke js

--- joke.js
+++ joke.js
@@ -5,4 +5,16 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     console.log(data.joke);
 
     joke.textContent = data.joke;
-})
+    BasicJoke.textContent = "Joke of the Day😹"
+});
+
+document.getElementById("programming").addEventListener("click", myFunction()){
+fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    console.log(data.programming);
+
+    
+});
+
