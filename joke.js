df60e2c111d8a231bfee8c4afda9bad3fb9c6df6
KylieAimee <kylie.farrell@edu.sait.ca>
2025-03-23 20:53:28 -0600

    add other joke js and font

--- joke.js
+++ joke.js
@@ -1,34 +1,76 @@
+//Basic Joke
 fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
 .then(response => response.json())
 .then(data => {
     console.log(data);
     console.log(data.joke);
 
-    NewJoke.textContent = data.joke;
+    NewJoke.textContent = data.joke || data.setup+ '...'+ data.delivery;
     JokeTitle.textContent = "Joke of the Day😹";
 });
 
 //programming Joke 
 document.getElementById("programming").addEventListener('click', function(){
-fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    console.log(data.joke);
-    JokeTitle.textContent = "Programming Joke"
-    NewJoke.textContent = data.joke || data.setup+ "..."+ data.delivery;
+    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.joke);
+        JokeTitle.textContent = "A Random Programming Joke"
+        NewJoke.textContent = data.joke || data.setup+ '...'+ data.delivery;
  
-});
-});
+    });
+    });
 
 
 //Miscellaneous Joke
-
+document.getElementById("miscellaneous").addEventListener('click', function(){
+    fetch("https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.joke);
+        JokeTitle.textContent = "A Random Miscellaneous Joke"
+        NewJoke.textContent = data.joke || data.setup+ '...'+ data.delivery;
+     
+    });
+    });
 
 //Pun joke
-
+document.getElementById("pun").addEventListener('click', function(){
+    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.joke);
+        JokeTitle.textContent = "A Random Pun"
+        NewJoke.textContent = data.joke || data.setup+ '...'+ data.delivery;
+     
+    });
+    });
 
 //Spooky Joke
-
+document.getElementById("spooky").addEventListener('click', function(){
+    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.joke);
+        JokeTitle.textContent = "A Random Spooky Joke"
+        NewJoke.textContent = data.joke || data.setup+ '...'+ data.delivery;
+     
+    });
+    });
 
 //Christmas Joke
+document.getElementById("christmas").addEventListener('click', function(){
+    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.joke);
+        JokeTitle.textContent = "A Random Christmas Joke"
+        NewJoke.textContent = data.joke || data.setup+ '...'+ data.delivery;
+     
+    });
+    });
